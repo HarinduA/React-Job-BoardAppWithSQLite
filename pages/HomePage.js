768f62aff2c2a7dc315cc2d63aa8f9b0f6331cf6
Harindu Adhikari <115646592+HarinduA@users.noreply.github.com>
2024-08-27 18:36:44 +0530

    Add files via upload

---
+++ pages/HomePage.js
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import JobList from '../components/JobList';
+import { useJobs } from '../lib/graphql/hooks';
+
+const JOBS_PER_PAGE = 5;
+
+function HomePage() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const { jobs, loading, error }
+    = useJobs(JOBS_PER_PAGE, (currentPage - 1) * JOBS_PER_PAGE);
+
+  console.log('[HomePage]', { jobs, loading, error });
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div className="has-text-danger">Data unavailable</div>;
+  }
+  return (
+    <div>
+      <h1 className="title">
+        Job Board
+      </h1>
+      <div>
+        <button onClick={() => setCurrentPage(currentPage - 1)}>
+          Previous
+        </button>
+        <span> {currentPage} </span>
+        <button onClick={() => setCurrentPage(currentPage + 1)}>
+          Next
+        </button>
+      </div>
+      <JobList jobs={jobs} />
+    </div>
+  );
+}
+
+export default HomePage;
