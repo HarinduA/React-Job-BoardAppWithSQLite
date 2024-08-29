4e87fc99ef87439a834326a119cf9cc483d62782
Harindu Adhikari <115646592+HarinduA@users.noreply.github.com>
2024-08-29 16:17:06 +0530

    hooks added

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import express from 'express';
 import {readFile } from 'node:fs/promises';
 import { authMiddleware, handleLogin } from './auth.js';
 import { resolvers } from './resolvers.js';
+import { getUser } from './db/users.js';
+import { createCompanyLoader } from './db/companies.js';
 
 const PORT = 9000;
 
@@ -15,8 +17,13 @@ app.post('/login', handleLogin);
 
 const typeDefs =  await readFile('./schema.graphql', 'utf8');
 
-function getContext({ req }) {
-  return { auth: req.auth};
+async function getContext({ req }) {
+  const companyLoader = createCompanyLoader();
+  const context = { companyLoader};
+  if (req.auth){
+    const user = await getUser(req.auth.sub);
+  }
+  return context;
 }
 
 const apolloServer = new ApolloServer ({ typeDefs, resolvers });
